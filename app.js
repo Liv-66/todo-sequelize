034f95c5ae0986f50e97aaed937cf68188ddb9a5
Liv Huang <livhuang95@gmail.com>
2020-11-22 19:53:29 +0800

    feat: add passport and modify login controller

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const methodOverride = require('method-override');
+const session = require('express-session');
 
+const usePassport = require('./config/passport');
 const db = require('./models');
 const Todo = db.Todo;
 const User = db.User;
@@ -11,9 +13,20 @@ const User = db.User;
 const app = express();
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+
+app.use(
+  session({
+    secret: 'ThisIsMySecret',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
+usePassport(app);
 
 app.get('/', async (req, res) => {
   try {
@@ -36,9 +49,14 @@ app.get('/users/login', (req, res) => {
   res.render('login');
 });
 
-app.post('/users/login', (req, res) => {
-  res.send('login');
-});
+const passport = require('passport');
+app.post(
+  '/users/login',
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/users/login',
+  })
+);
 
 app.get('/users/signup', (req, res) => {
   res.render('signup');
